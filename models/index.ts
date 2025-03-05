2a7e60c98199bffa2911a1654cda0ad910dfda48
4ric4 <ricardocavalcantecs@gmail.com>
2025-03-05 20:30:36 -0300

    uploads arquivos

--- models/index.ts
+++ models/index.ts
@@ -1,12 +1,16 @@
 import { Category } from "./category";
 import { Course } from "./Course";
+import { Episode } from './Episode'
 
 
 Category.hasMany(Course)
+Course.hasMany(Episode)
 
 Course.belongsTo(Category)
+Episode.belongsTo(Course)
 
 export{
     Category,
-    Course
+    Course,
+    Episode
 }
