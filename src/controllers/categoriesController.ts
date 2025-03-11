b7f86ce1dc95750492f1522eb93c433be0bdc858
4ric4 <ricardocavalcantecs@gmail.com>
2025-03-11 18:58:11 -0300

    adicionando rota de cursos

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -4,6 +4,7 @@ import { categoryService } from '../services/categoryService'
 import { getPaginationParams } from '../helpers/getPAginationParams'
 
 export const categoriesController = {
+    //get /categories
     index: async (req: Request, res: Response) =>{
         const [page,perPage] = getPaginationParams(req.query)
 
@@ -11,6 +12,21 @@ export const categoriesController = {
             const paginatedCategories = await categoryService.findAllPaginated(page,perPage)
             return res.json({  paginatedCategories})           
         } catch (err){
+            if (err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+
+    //get /categories/:id
+
+    show: async (req:Request, res: Response) => {
+        const { id } = req.params
+
+        try {
+            const category = await categoryService.findByIdWithCourses(id)
+            return res.json(category)
+        } catch (err) {
             if (err instanceof Error){
                 return res.status(400).json({message: err.message})
             }
